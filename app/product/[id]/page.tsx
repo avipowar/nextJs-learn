e63c127424f28b645c0704044e5d32128fb6423e
avipowar <powaravinash06/2gmail.com>
2026-06-09 17:03:05 +0530

    learn how to read dynamic id from url

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const ProductIdPage = () => {
-  return <div>ProductIdPage</div>;
+const ProductIdPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await params;
+  return <div>ProductIdPage{id}</div>;
 };
 
 export default ProductIdPage;
