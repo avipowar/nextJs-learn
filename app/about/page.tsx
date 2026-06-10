bea6a3a80f9852b4dbe8baedc614b3c2bc0f37f3
avipowar <powaravinash06/2gmail.com>
2026-06-10 12:26:08 +0530

    link componenet

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Link from "next/link";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      AboutPage
+      <Link href="../contact">Contaxt page</Link>
+    </div>
+  );
 };
 
 export default AboutPage;
