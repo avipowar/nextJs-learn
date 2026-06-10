e0b7e15c239b14696353f1190442a8a8d41d24fd
avipowar <powaravinash06/2gmail.com>
2026-06-10 12:35:09 +0530

    learn image component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div>
+      ContactPage
+      <Image
+        src="https://chaicode.com/assets/white-1-CYshgcRl.webp"
+        width={500}
+        height={500}
+        alt="Picture of the author"
+      />
+    </div>
+  );
 };
 
 export default ContactPage;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "chaicode.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
