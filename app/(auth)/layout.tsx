7919c7da78121ccf3b08d6a70993f696369f09cf
avipowar <powaravinash06/2gmail.com>
2026-06-10 11:33:43 +0530

    leran how to apply nasted layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// @ts-ignore
+const AuthLayoutPage = ({ children }) => {
+  return (
+    <div>
+      <h1>AuthLayoutPage</h1>
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayoutPage;
