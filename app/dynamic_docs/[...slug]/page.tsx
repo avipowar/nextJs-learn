9a4d9543897fad6c3c1655dca8a0832a077d5701
avipowar <powaravinash06/2gmail.com>
2026-06-10 11:07:17 +0530

    catch all routes '

---
+++ app/dynamic_docs/[...slug]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// @ts-ignore
+const DynamicDocumentPage = async ({ params }) => {
+  const { slug } = await params;
+  return <div>DynamicDocumentPage, {slug.join("/")}</div>;
+};
+
+export default DynamicDocumentPage;
