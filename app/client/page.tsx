5b16c00ad63b493554f60ebb6173830f96fb9c0d
avipowar <powaravinash06/2gmail.com>
2026-06-11 13:23:17 +0530

    see the diff of client side conponent and server side component

---
+++ app/client/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+const ClientSidePage = () => {
+  const [data, setdata] = useState(null);
+
+  useEffect(() => {
+    async function fetchdata() {
+      const res = await fetch(
+        "https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10",
+      );
+      const data = await res.json();
+      setdata(data);
+      console.log(data);
+    }
+    fetchdata();
+  }, []);
+
+  return <div>ClientSidePage</div>;
+};
+
+export default ClientSidePage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch(
+    "https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10",
+  );
+  const orginalData = await data.json();
+  console.log(data);
   return (
     <div>
       <h1>hello world</h1>
